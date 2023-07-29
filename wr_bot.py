965cd389a3627d48f54165cf29a2ec059ae54c6c
Alexander.Chad <Alexander.Chad@yandex.ru>
2023-07-29 23:17:30 +0700

    добавлен таймаут для рекламы и отдельная проверка активности кнопки в специальном меню

--- wr_bot.py
+++ wr_bot.py
@@ -8,11 +8,15 @@ import numpy as np
 work_dir:str #рабочая директория
 screenshot_path:str #пусть к скриншоту
 img_rgb:np.ndarray # скриншот
-target_images:np.ndarray=['ad_disable', 'ad_enable', 'black_market', 'ad_exit_next0', 'ad_exit_next1', 'ad_exit_next2', 'ad_exit0', 'ad_exit1', 'ad_exit2', 'box_open', 'main_menu', 'main_menu_exit', 'discount_banner0', 'discount_banner1', 'discount_banner2', 'back', 'bronze_box']
+target_images:np.ndarray=['ad_enable', 'get', 'ok', 'black_market', 'discount_special', 'ad_exit_next0', 'ad_exit_next1', 'ad_exit_next2', 'ad_exit0', 'ad_exit1', 'ad_exit2', 'box_open', 'main_menu', 'main_menu_exit', 'discount_banner0', 'discount_banner1', 'discount_banner2', 'back', 'bronze_box']
 target_images_rgb={} #загруженные целевые изображения
 target_images_psize={} #половинный размеры загруженных целевых изображений
 target_recognized={} #распознанные целевые изображения на скриншоте
-threshold = 0.7 #порог распознавания
+threshold=0.8 #порог распознавания
+discount_special_cn=0 #пункт меню специальных предложений
+start_time_ad:time #время начала видео
+timeout_ad=30 #время, отведенное на рекламу
+ad_mode=False
 
 def printLog(str_log):
     print(time.strftime("%X\t", time.localtime()) + str_log)
@@ -50,7 +54,11 @@ def recognize_screenshot():
         cy=max_loc[1]+target_images_psize[img_t][1]
         #записываем результаты
         target_recognized[img_t]=[True if (max_val>threshold) else False, cx, cy]
-        printLog(f"Img: {img_t} - {target_recognized[img_t][0]}, location: x:{cx}, y:{cy}")
+        if img_t=='ad_enable' and sum(img_rgb[cy][cx-158])<700: #проверяем активна ли кнопка
+            target_recognized[img_t][0]=False
+            printLog(f"Img: ad_enable - disabled, location: x:{cx}, y:{cy}")
+        if target_recognized[img_t][0]:
+            printLog(f"Img: {img_t}, location: x:{cx}, y:{cy}")
     printLog("recognize_screenshot is complete.")
 
 def tap_screen(x, y):
@@ -58,23 +66,56 @@ def tap_screen(x, y):
     printLog(f"tap x:{x}, y:{y}")
 
 def au_worker():
+    global discount_special_cn, start_time_ad, ad_mode
     if (target_recognized['discount_banner0'][0] or target_recognized['discount_banner1'][0]) and target_recognized['main_menu_exit'][0]: #первый тип акционного банера, закрываем
         tap_screen(target_recognized['main_menu_exit'][1], target_recognized['main_menu_exit'][2])
     elif target_recognized['discount_banner2'][0]: #второй тип акционного банера, закрываем
         tap_screen(1764, 267)
-    elif target_recognized['main_menu'][0]: #если в начальном меню, то идем в коробки
-        tap_screen(62, 720)
+    elif target_recognized['main_menu'][0]: #если в начальном меню, то
+        if discount_special_cn<5: #уже были в специальном?
+            tap_screen(190, 470) #идем в специальное
+        else:
+            tap_screen(62, 720) #идем в коробки
     elif target_recognized['box_open'][0]: #если можем открыть коробку
         tap_screen(652, 744)
+        start_time_ad = time.time()
+        ad_mode=True
+    elif target_recognized['get'][0]: #если можем получить вознаграждение
+        tap_screen(target_recognized['get'][1], target_recognized['get'][2])
+        ad_mode=False
+    elif target_recognized['ok'][0]: #подтверждаем получение вознаграждения
+        tap_screen(target_recognized['ok'][1], target_recognized['ok'][2])
+    elif target_recognized['discount_special'][0]: #если в специальном
+        if target_recognized['ad_enable'][0]: #активна кнопка "смотреть"
+            tap_screen(target_recognized['ad_enable'][1], target_recognized['ad_enable'][2])
+            start_time_ad = time.time()
+            ad_mode=True
+        else: #нет для открытия коробок, то открываем следующую вкладку
+            if discount_special_cn==1: 
+                tap_screen(80, 370) #вторая вкладка
+            elif discount_special_cn==2:
+                tap_screen(80, 470) #третья вкладка
+            elif discount_special_cn==3:
+                tap_screen(80, 560) #четвертая вкладка
+            elif discount_special_cn==4:
+                tap_screen(80, 1024) #выход в главное меню
+            discount_special_cn+=1
     elif target_recognized['black_market'][0]: #если нельзя открыть бесплатно коробку (мы в меню коробок), то завершаем скрипт
         printLog("black_market closed. Exit.")
         sys.exit()  # завершаем программу
     elif target_recognized['back'][0] and target_recognized['bronze_box'][0]: #если покрутили рулетку, то выходим из коробки
         tap_screen(100, 1024)
+        ad_mode=False
     else:
+        tap_exit_ad=False
         for ad_e in {'ad_exit_next0', 'ad_exit_next1', 'ad_exit_next2', 'ad_exit0', 'ad_exit1', 'ad_exit2'}: #ищем кнопки для выхода из рекламы
             if target_recognized[ad_e][0]:
                 tap_screen(target_recognized[ad_e][1], target_recognized[ad_e][2])
+                tap_exit_ad=True
+                break
+        if ad_mode and (not tap_exit_ad) and ((time.time() - start_time_ad)>timeout_ad): #если не нажимали на выход и время на рекламу вышло, а мы все еще в режиме просмотра
+            tap_screen(2200, 50) #жмем в ту область, где он должен быть
+
 
 if __name__ == "__main__":
     printLog("Starting wr_bot")
